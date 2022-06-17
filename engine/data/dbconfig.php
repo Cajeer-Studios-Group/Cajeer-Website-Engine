65ecb3f20232ebf2b1e9e7fba29e547956ba34a2
Dmitry Efimov <101918317+TheSkiF4er@users.noreply.github.com>
2022-06-17 20:25:15 +1000

    Create dbconfig.php

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+define ("DBHOST", "localhost"); 
+define ("DBNAME", "cwe");
+define ("DBUSER", "root");
+define ("DBPASS", "");  
+define ("PREFIX", "cwe"); 
+define ("USERPREFIX", "cwe");
+define ("COLLATE", "cp1251"); 
+?>
